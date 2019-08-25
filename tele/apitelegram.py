b6f47aa241214ef964637ad63b9216278b52440f
jalejandrorangel <alex_rangel@comunidad.unam.mx>
2019-08-25 01:41:23 -0500

    telegram

---
+++ tele/apitelegram.py
@@ -0,0 +1,17 @@
+import telegram
+bot = telegram.Bot(token='836491530:AAGgdH1ljzRfwTLiFXu29X7aY1lCdZogFQQ')
+print(bot.get_me())
+updates = bot.get_updates()
+#input()
+print([(u.message.chat.id, u.message.chat.first_name) for u in updates])
+updates = bot.get_updates()
+print([u.message.photo for u in updates if u.message.photo])
+chat_id = bot.get_updates()[-1].message.chat_id
+#chat = bot.chat
+#print(chat_id)
+#chat_id = "MarcusDaOz"
+#chat_id=878793109
+chat_id = int(input())
+cad = 
+#bot.send_message(chat_id=chat_id, text="Te espero bebe")
+bot.send_photo(chat_id=chat_id, photo=open('logo.png','rb'))

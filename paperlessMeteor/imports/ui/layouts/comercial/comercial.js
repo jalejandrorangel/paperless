9401c67f595d58d6041cd99af231b195161380a2
ivan19k <ivan.felavel@gmail.com>
2019-08-24 23:52:17 -0500

    principales vistas

---
+++ paperlessMeteor/imports/ui/layouts/comercial/comercial.js
@@ -0,0 +1,8 @@
+// import { $ } from 'meteor/jquery';
+import { Template } from 'meteor/templating';
+import './comercial.html';
+
+Template.comercial.events
+({
+    
+});
---
+++ paperlessMeteor/imports/ui/layouts/contratos/contratos.js
@@ -0,0 +1,8 @@
+// import { $ } from 'meteor/jquery';
+import { Template } from 'meteor/templating';
+import './contratos.html';
+
+Template.contratos.events
+({
+    
+});

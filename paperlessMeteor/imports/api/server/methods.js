7a51e4d0bd9c22905cc10503d7cfc0515ee31681
Ivan <ivan.felavel@gmail.com>
2019-08-25 10:52:44 -0500

    nuevo_commit

--- paperlessMeteor/imports/api/server/methods.js
+++ paperlessMeteor/imports/api/server/methods.js
@@ -8,9 +8,11 @@ import { Usuarios } from '../documentos/documentos.js';
 //bancoazteca  99aa05cdb0d69feb86f655d24d2ad855-2ae2c6f3-f16e909e
 Meteor.methods ({     
     enviar_email(datos) {
-		process.env.MAIL_URL="smtps://bancoazteca%40mail.juezguapa.com:99aa05cdb0d69feb86f655d24d2ad855-2ae2c6f3-f16e909e@smtp.mailgun.org:465/";
+		/*process.env.MAIL_URL="smtps://bancoazteca%40mail.juezguapa.com:99aa05cdb0d69feb86f655d24d2ad855-2ae2c6f3-f16e909e@smtp.mailgun.org:465/";*/
+        process.env.MAIL_URL="smtps://juezguapa%40gmail.com:acmrules@smtp.gmail.com:465/";
         const to = datos.email;
-        const from = 'bancoazteca@mail.juezguapa.com';
+        /*const from = 'bancoazteca@mail.juezguapa.com';*/
+        const from = 'juezguapa@gmail.com';
         const subject = 'Notificacion BAz';
         const text = datos.text;
         Email.send({ to, from, subject, text }); 
@@ -34,24 +36,22 @@ Meteor.methods ({
 			telnum = "9965415";
 		else if (u.numero == "5560653195")
 			telnum = "119478126";
-		for (key in u.preferencias) {
-			if (u.preferencias.sms) {
-				Documentos.insert({tipo:'sms', texto:'Guarda tu contrato', numero: u.numero})
-			}
-			if (u.preferencias.whatsapp) {
-				Documentos.insert({tipo:'whatsapp', texto:'Guarda tu contrato', numero: u.numero,
-					url:'https://uva.onlinejudge.org/external/122/12279.pdf'})
-			}
-			if (u.preferencias.email) {
-				Meteor.call('enviar_email', {email:u.email, text:'Guarda tu contrato'});
-			}
-			if (u.preferencias.telegram) {
-				Documentos.insert({tipo:'telegram', texto:'Guarda tu contrato', numero: telnum})
-			}
-			if (u.preferencias.messenger) {
-				Documentos.insert({tipo:'messenger', texto:'Guarda tu contrato', numero: telnum})
-			}
 
+		if (u.preferencias.sms) {
+			Documentos.insert({tipo:'sms', texto:'Guarda tu contrato', numero: u.numero})
+		}
+		if (u.preferencias.whatsapp) {
+			Documentos.insert({tipo:'whatsapp', texto:'Guarda tu contrato', numero: u.numero,
+				url:'https://uva.onlinejudge.org/external/122/12279.pdf'})
+		}
+		if (u.preferencias.email) {
+			Meteor.call('enviar_email', {email:u.email, text:'Guarda tu contrato'});
+		}
+		if (u.preferencias.telegram) {
+			Documentos.insert({tipo:'telegram', texto:'Guarda tu contrato', numero: telnum})
+		}
+		if (u.preferencias.messenger) {
+			Documentos.insert({tipo:'messenger', texto:'Guarda tu contrato', numero: telnum})
 		}
 	},
 
@@ -63,29 +63,26 @@ Meteor.methods ({
 			telnum = "9965415";
 		else if (u.numero == "5560653195")
 			telnum = "119478126";
-		for (key in u.preferencias) {
-			if (u.preferencias.sms) {
-				Documentos.insert({tipo:'sms', texto:'Hubo un retiro de' + datos.monto + " de la tarjeta " +
-					datos.tarjeta, 
-					numero: u.numero})
-			}
-			if (u.preferencias.whatsapp) {
-				Documentos.insert({tipo:'whatsapp', texto:'Hubo un retiro de' + datos.monto + " de la tarjeta " +
-					datos.tarjeta, numero: u.numero,})
-			}
-			if (u.preferencias.email) {
-				Meteor.call('enviar_email', {email:u.email, text:'Hubo un retiro de' + datos.monto + " de la tarjeta " +
-					datos.tarjeta});
-			}
-			if (u.preferencias.telegram) {
-				Documentos.insert({tipo:'telegram', texto:'Hubo un retiro de' + datos.monto + " de la tarjeta " +
-					datos.tarjeta, numero: telnum})
-			}
-			if (u.preferencias.messenger) {
-				Documentos.insert({tipo:'messenger', texto:'Hubo un retiro de ' + datos.monto + " de la tarjeta " +
-					datos.tarjeta, numero: telnum})
-			}
-
+		if (u.preferencias.sms) {
+			Documentos.insert({tipo:'sms', texto:'Hubo un retiro de' + datos.monto + " de la tarjeta " +
+				datos.tarjeta, 
+				numero: u.numero})
+		}
+		if (u.preferencias.whatsapp) {
+			Documentos.insert({tipo:'whatsapp', texto:'Hubo un retiro de' + datos.monto + " de la tarjeta " +
+				datos.tarjeta, numero: u.numero,})
+		}
+		if (u.preferencias.email) {
+			Meteor.call('enviar_email', {email:u.email, text:'Hubo un retiro de' + datos.monto + " de la tarjeta " +
+				datos.tarjeta});
+		}
+		if (u.preferencias.telegram) {
+			Documentos.insert({tipo:'telegram', texto:'Hubo un retiro de' + datos.monto + " de la tarjeta " +
+				datos.tarjeta, numero: telnum})
+		}
+		if (u.preferencias.messenger) {
+			Documentos.insert({tipo:'messenger', texto:'Hubo un retiro de ' + datos.monto + " de la tarjeta " +
+				datos.tarjeta, numero: telnum})
 		}
 	},
 
--- tele/tele.py
+++ tele/tele.py
@@ -7,7 +7,7 @@ while True:
         data.append(d)
     except EOFError:
         break
-print("simon")
+print("simon {}".format(d))
 bot = telegram.Bot(token='836491530:AAGgdH1ljzRfwTLiFXu29X7aY1lCdZogFQQ')
 chat_id = data[0]
 bot.send_message(chat_id=chat_id, text=data[1])
